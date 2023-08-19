48ec1eb6cca0177d32984d60889c9232b6cfaad1
AoPong-831 <newipadmail114514@gmail.com>
2023-08-19 15:06:41 +0900

    0.35 google map API 使えるようになる

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 from flask import Flask,render_template, request, redirect, Response, make_response
+import googlemaps
+
 app = Flask(__name__)
 
+GoogleApiKey = 'AIzaSyANuDUtyjaATaB2wXaJzo4MQI7XXu9Rbxg'
+gmaps = googlemaps.Client(key=GoogleApiKey)
 
+def Geocoding(name):
+    result = gmaps.geocode(name)
+    lat = result[0]["geometry"]["location"]["lat"]#緯度
+    lng = result[0]["geometry"]["location"]["lng"]#軽度
+    return [name,lat,lng]
 
 @app.route('/',methods=["GET","POST"])
 def index():
     if request.method == "POST":
-        name = request.form["entry"]
+        name = request.form["entry"]#名前を受け取る
 
-        print("="*100)
-        print(name)
+        print(Geocoding(name))
         return redirect("/")
     else:
         return render_template("index.html")
