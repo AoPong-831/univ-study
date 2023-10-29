befaf1cd1c0265679b7dfd3d8d75b72815218df6
椿大翔 <izanami@tsubakihirotonoMacBook-Air.local>
2023-10-29 22:36:44 +0900

    yolov5を使って物体認識をできるようにした(簡易)。

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import cv2
 import datetime
 import numpy as np
 import easyocr
+from yolov5 import detect
+import subprocess
+import os
+import shutil
+import time
 
 app = Flask(__name__)
 
@@ -29,24 +34,50 @@ def index():
 
     if request.method == "GET": img_path = None
     elif request.method == "POST":
+        #imgsの中身を一旦空にする(imgsを消して、再度作成)
+        shutil.rmtree('static/imgs')
+        os.mkdir('static/imgs')
+
         #POSTにより受け取った画像を読み込む
         #---よくわからないc&p zone(htmlから画像を読み込み、pathを返す)
         stream = request.files["img"].stream
         img_array = np.asarray(bytearray(stream.read()),dtype=np.uint8)
         img = cv2.imdecode(img_array,1)#画像データ(配列)
         #---よくわからないc&p zone 終わり
-        #現在時刻を名前として「imgs/」に保存する
+
+        #***現在時刻を名前として「imgs/」に保存する
         dt_now = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
         img_path = img_dir + dt_now + ".jpg"
         cv2.imwrite(img_path, img)#画像保存を行う関数(ファイル名,多次元配列(numpy,ndarray))
+        #***
 
         #画像処理(easyocr)
         reader = easyocr.Reader(["ja","en"])
-        result = reader.readtext(img_path,detail=0)
+        result = reader.readtext(img_path,detail=0)#文字に直す？
 
         #geocoding
-        data = Geocoding(result[0])#data = Geocoding(name)
+        try:
+            data = Geocoding(result[0])#data = Geocoding(name)
+        except:#Geocording で何かしらのエラーが起こった時。
+            data = ['none', 0, 0]
+
+    #保存した画像ファイルのpath, geocordingデータをHTMLに渡す
     return render_template("index.html", img_path = img_path,name=data[0],lat=data[1],lng=data[2],API=GoogleApiKey)
 
+
+@app.route('/yolov5', methods=['GET'])
+def yolov5():
+    #detectの中身を一旦空にする(detectを消して、再度作成)(imgsと同様の動作)
+    shutil.rmtree('yolov5/runs/detect')
+    os.mkdir('yolov5/runs/detect')
+
+    #yolov5
+    files = os.listdir("static/imgs")#指定pathの中身をlistで取得。(該当画像)
+    cmd = "python yolov5/detect.py --source static/imgs/"+files[0]#cmdを書く(detect.pyを動かすため)
+    subprocess.call(cmd.split())#cmd実行
+
+    return index()
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8888)
